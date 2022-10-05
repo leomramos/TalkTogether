c78d3b9862f32de1179dcab7f5fed03c8b5aec3b
Leonardo Ramos <50186247+leomramos@users.noreply.github.com>
2022-10-05 14:16:12 -0300

    configuring i18n

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import I18n from "./src/i18n";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{I18n.t("greeting")}</Text>
       <StatusBar style="auto" />
     </View>
   );
