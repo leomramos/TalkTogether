0fb1430879cc7b30b150cd45b94085ac9377dc02
Leonardo Ramos <50186247+leomramos@users.noreply.github.com>
2022-10-12 16:31:18 -0300

    configured color palette

--- mobile/src/utils/themes.js
+++ mobile/src/utils/themes.js
@@ -3,26 +3,43 @@ import { DefaultTheme } from "react-native-paper";
 const common = {
   ...DefaultTheme,
   roundness: 5,
+  user: {
+    avatar: {
+      white: "#D9D9D9",
+      gray: "#9D9B9B",
+      salmon: "#A39090",
+      yellow: "#ABA496",
+      green: "#9DAB96",
+      cyan: "#96ABAA",
+      blue: "#969BAB",
+      violet: "#A796AB",
+      orange: "#AB9696",
+    },
+  },
   screen: {
     paddingLeft: "20px",
     paddingRight: "20px",
   },
-  text: {
-    pageTitle: {
-      font: "Inter_700Bold",
-      size: "26px",
+  typography: {
+    tab: {
+      title: {
+        font: "Inter_700Bold",
+        size: "26px",
+      },
     },
     input: {
       font: "Inter_500Medium",
       size: "14px",
     },
-    overlayTitle: {
-      font: "Inter_600SemiBold",
-      size: "16px",
-    },
-    overlayFooter: {
-      font: "Inter_600SemiBold",
-      size: "14px",
+    overlay: {
+      title: {
+        font: "Inter_600SemiBold",
+        size: "16px",
+      },
+      footer: {
+        font: "Inter_600SemiBold",
+        size: "14px",
+      },
     },
   },
   colors: {
@@ -33,38 +50,29 @@ const common = {
 const darkTheme = {
   ...common,
   dark: true,
-  borderColor: "#5A189A",
-  text: {
-    ...common.text,
-    icons: {
-      idle: "#909095",
-      active: "#FDFDFD",
-    },
-    pageTitle: {
-      ...common.text.pageTitle,
-      color: "#FDFDFD",
-    },
-    input: {
-      ...common.text.input,
-      color: "#CECED0",
-      placeholder: "#76767C",
-    },
-    overlayTitle: {
-      ...common.text.overlayTitle,
-      color: "#FDFDFD",
-    },
-    overlayFooter: {
-      ...common.text.overlayFooter,
-      color: "#CECED0",
-    },
-  },
   colors: {
     ...common.colors,
-    background: "#21212B",
-    focused: "#9D4EDD",
-    unfocused: "#909095",
-    backgroundAccent: "#181820",
-    surface: "#272732",
+    purple: {
+      first: "#10002B",
+      second: "#240046",
+      third: "#3C096C",
+      fourth: "#5A189A",
+      fifth: "#7B2CBF",
+      sixth: "#9D4EDD",
+      seventh: "#C77DFF",
+      eighth: "#E0AAFF",
+    },
+    gray: {
+      first: "#FDFDFD",
+      second: "#CECED0",
+      third: "#909095",
+      fourth: "#76767C",
+      fifth: "#414052",
+      sixth: "#292839",
+      seventh: "#272732",
+      eighth: "#21212B",
+      ninth: "#181820",
+    },
   },
 };
 
